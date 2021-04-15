4ab2c3f6dcec2e82ca24355680b0d6ecfed299e0
SanusiMD <just4deen@yahoo.com>
2021-04-15 14:32:24 +0100

    Add Uppercase Eventlistener

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
-const inputText = document.querySelector('.convertForm')
-const upperCase = document.querySelector('.upperBtn')
-const lowerCase = document.querySelector('.lowerBtn')
-const sentenceCase = document.querySelector('.sentenceBtn')
-const inputText = document.querySelector('.clearBtn')
+const inputText = document.querySelector("#convertForm");
+const upperCase = document.querySelector(".upperBtn");
+const lowerCase = document.querySelector(".lowerBtn");
+const sentenceCase = document.querySelector(".sentenceBtn");
+const clearButton = document.querySelector(".clearBtn");
+
+// Upper Case 
+upperCase.addEventListener("click", function () {
+  const inputValue = inputText.value
+  const caseUpper = inputValue.toLocaleUpperCase();
+  document.querySelector("#convertForm").value = caseUpper
+   
+});
+
+
