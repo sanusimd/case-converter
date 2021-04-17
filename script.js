dd475c057ba26e8cde39f4d1fc18dded03782dac
SanusiMD <just4deen@yahoo.com>
2021-04-17 17:39:51 +0100

    Add clear and sentence case  functionality

--- script.js
+++ script.js
@@ -4,12 +4,29 @@ const lowerCase = document.querySelector(".lowerBtn");
 const sentenceCase = document.querySelector(".sentenceBtn");
 const clearButton = document.querySelector(".clearBtn");
 
-// Upper Case 
+// Upper Case Event Listener
 upperCase.addEventListener("click", function () {
-  const inputValue = inputText.value
+  const inputValue = inputText.value;
   const caseUpper = inputValue.toLocaleUpperCase();
-  document.querySelector("#convertForm").value = caseUpper
-   
+  document.querySelector("#convertForm").value = caseUpper;
+});
+
+// Lower Case Event Listener
+lowerCase.addEventListener("click", function () {
+  const inputValue = inputText.value;
+  const caseLower = inputValue.toLowerCase();
+  document.querySelector("#convertForm").value = caseLower;
+});
+
+// Sentence Case Event Listener
+sentenceCase.addEventListener("click", function () {
+  const inputValue = inputText.value;
+  const senCase = inputValue.charAt(0).toUpperCase() + inputValue.slice(1);
+  document.querySelector("#convertForm").value = senCase;
 });
 
 
+// Clear button Event listener
+clearButton.addEventListener("click", function () {
+  inputText.value = " Type or Paste Your Text Here To Convert Case... ";
+});
